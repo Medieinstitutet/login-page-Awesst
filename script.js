5f1c1cf3f31ed419361ceae88fa1dc5522b8a54a
Jakob Dahlberg <dahlbergjakob@hotmail.com>
2022-11-21 18:39:14 +0100

    start

---
+++ script.js
@@ -0,0 +1,14 @@
+// Global Variables, username, password
+let userName = document.getElementById("userName");
+let passWord = document.getElementById("passWord");
+let submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("Click", () => {
+  console.log("Hello Bitch");
+});
+
+// DOM for logged in & logged out.
+
+// Form Submit log in.
+
+// localStorage Saving.
