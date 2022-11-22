0ba1632ad8cb6cf0cb644c4941e43824238d9397
Jakob Dahlberg <dahlbergjakob@hotmail.com>
2022-11-22 15:48:41 +0100

    global variables + evenlistener + scroll function

--- script.js
+++ script.js
@@ -2,13 +2,45 @@
 let userName = document.getElementById("userName");
 let passWord = document.getElementById("passWord");
 let submitBtn = document.getElementById("submitBtn");
+let secondPage = document.getElementById("secondPage");
+let thirdPage = document.getElementById("thirdPage");
+let login = document.getElementById("login");
+let logout = document.getElementById("logout");
+let loginForm = document.getElementById("loginForm");
 
-submitBtn.addEventListener("Click", () => {
-  console.log("Hello Bitch");
+// Eventlisteners
+submitBtn.addEventListener("click", () => {
+  secondPage.style.display = "block";
+  thirdPage.style.display = "block";
+  login.style.display = "none";
+  logout.style.display = "block";
+  loginForm.style.display = "none";
 });
 
-// DOM for logged in & logged out.
-
-// Form Submit log in.
+login.addEventListener("click", () => {
+  loginForm.style.display = "flex";
+});
+logout.addEventListener("click", () => {
+  secondPage.style.display = "none";
+  thirdPage.style.display = "none";
+  logout.style.display = "none";
+  login.style.display = "block";
+  loginForm.style.display = "none";
+});
 
 // localStorage Saving.
+
+//  STICKY HEADER
+
+window.onscroll = function () {
+  myFunction();
+};
+let header = document.getElementById("header");
+let sticky = header.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
