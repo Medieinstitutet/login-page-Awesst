15d4538ea3213d10c925c13aef43dec81f385a0e
Jakob Dahlberg <dahlbergjakob@hotmail.com>
2022-11-23 20:12:56 +0100

    tweeks, new button

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
-// Global Variables, username, password
-let userName = document.getElementById("userName");
-let passWord = document.getElementById("passWord");
+// Global Variables, userName, passWord
 let submitBtn = document.getElementById("submitBtn");
-let secondPage = document.getElementById("secondPage");
-let thirdPage = document.getElementById("thirdPage");
+let aboutUs = document.getElementById("aboutUs");
+let products = document.getElementById("products");
 let login = document.getElementById("login");
 let logout = document.getElementById("logout");
 let loginForm = document.getElementById("loginForm");
+const userName = document.getElementById("userName");
+const passWord = document.getElementById("passWord");
+
+const users = [
+  { userName: "janne", passWord: "test" },
+  { userName: "jakob", passWord: "dahlberg" },
+];
 
 // Eventlisteners
 submitBtn.addEventListener("click", () => {
-  secondPage.style.display = "block";
-  thirdPage.style.display = "block";
+  let user = users.find(
+    (user) =>
+      user.userName === userName.value && user.passWord === passWord.value
+  );
+  if (user) {
+    loginUser();
+    localStorage.setItem("userLoggedIn", user.userName);
+  } else {
+    alert("invalid");
+  }
+});
+const checkPassword = (passWord) => {
+  let specialChars = ["$", "€", 1 - 9, "@", "!", "#", "¤", "%", "/", "(", ")"];
+  if (passWord.value.match(specialChars)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const loginUser = () => {
+  aboutUs.style.display = "block";
+  products.style.display = "block";
   login.style.display = "none";
   logout.style.display = "block";
   loginForm.style.display = "none";
-});
+};
+
+if (localStorage.getItem("userLoggedIn") === "true") {
+  loginUser();
+}
 
 login.addEventListener("click", () => {
   loginForm.style.display = "flex";
 });
 logout.addEventListener("click", () => {
-  secondPage.style.display = "none";
-  thirdPage.style.display = "none";
+  logOutUser();
+});
+
+const logOutUser = () => {
+  aboutUs.style.display = "none";
+  products.style.display = "none";
   logout.style.display = "none";
   login.style.display = "block";
   loginForm.style.display = "none";
-});
+  localStorage.removeItem("userLoggedIn");
+};
 
 // localStorage Saving.
 
