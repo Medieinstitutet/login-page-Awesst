e3159ecf8dd845f13c3bc4407e105c398ead4d84
Jakob Dahlberg <dahlbergjakob@hotmail.com>
2022-11-25 23:21:56 +0100

    progress, reg

--- script.js
+++ script.js
@@ -7,12 +7,18 @@ let logout = document.getElementById("logout");
 let loginForm = document.getElementById("loginForm");
 const userName = document.getElementById("userName");
 const passWord = document.getElementById("passWord");
+let registerBtn = document.getElementById("registerBtn");
+let errorMsg = document.getElementById("error-msg");
+let headline = document.getElementById("headline");
 
 const users = [
   { userName: "janne", passWord: "test" },
   { userName: "jakob", passWord: "dahlberg" },
 ];
 
+// localStorage.setItem("users", JSON.stringify(users));
+
+// localStorage.setItem(users);
 // Eventlisteners
 submitBtn.addEventListener("click", () => {
   let user = users.find(
@@ -20,20 +26,33 @@ submitBtn.addEventListener("click", () => {
       user.userName === userName.value && user.passWord === passWord.value
   );
   if (user) {
-    loginUser();
     localStorage.setItem("userLoggedIn", user.userName);
+    loginUser();
   } else {
-    alert("invalid");
+    errorMsg.innerHTML = "Invalid Login.";
+    errorMsg.style.display = "block";
   }
 });
-const checkPassword = (passWord) => {
-  let specialChars = ["$", "€", 1 - 9, "@", "!", "#", "¤", "%", "/", "(", ")"];
-  if (passWord.value.match(specialChars)) {
-    return true;
-  } else {
-    return false;
-  }
-};
+login.addEventListener("click", () => {
+  loginForm.style.display = "flex";
+});
+logout.addEventListener("click", () => {
+  logOutUser();
+});
+let esc = document.getElementById("esc").addEventListener("click", () => {
+  loginForm.style.display = "none";
+  errorMsg.style.display = "none";
+});
+
+registerBtn.addEventListener("click", () => {
+  users.push({ userName: userName.value, passWord: passWord.value });
+  // localStorage.setItem("users", JSON.stringify(users));
+  // console.log(JSON.parse(localStorage.getItem("users")));
+  console.log("click");
+  console.log(users);
+});
+
+// Functions
 
 const loginUser = () => {
   aboutUs.style.display = "block";
@@ -41,18 +60,10 @@ const loginUser = () => {
   login.style.display = "none";
   logout.style.display = "block";
   loginForm.style.display = "none";
+  headline.style.display = "flex";
+  let userLoggedIn = localStorage.getItem("userLoggedIn");
+  headline.innerHTML = `Welcome ${userLoggedIn}`;
 };
-
-if (localStorage.getItem("userLoggedIn") === "true") {
-  loginUser();
-}
-
-login.addEventListener("click", () => {
-  loginForm.style.display = "flex";
-});
-logout.addEventListener("click", () => {
-  logOutUser();
-});
 
 const logOutUser = () => {
   aboutUs.style.display = "none";
@@ -61,9 +72,45 @@ const logOutUser = () => {
   login.style.display = "block";
   loginForm.style.display = "none";
   localStorage.removeItem("userLoggedIn");
+  errorMsg.style.display = "none";
+  headline.style.display = "none";
 };
 
+const isUserLoggedIn = () => {
+  loginForm.style.display = "none";
+  logout.style.display = "block";
+  login.style.display = "none";
+  aboutUs.style.display = "block";
+  products.style.display = "block";
+  let userLoggedIn = localStorage.getItem("userLoggedIn");
+  headline.innerHTML = `Welcome ${userLoggedIn}`;
+  headline.style.display = "flex";
+};
+
+checkIfLoggedIn = () => {
+  if (localStorage.getItem("userLoggedIn")) {
+    isUserLoggedIn();
+  }
+};
+checkIfLoggedIn();
 // localStorage Saving.
+
+// const checkPassword = () => {
+//   let specialChars = ["$", "€", 1 - 9, "@", "!", "#", "¤", "%", "/", "(", ")"];
+//   if (passWord.value.match(specialChars)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// const register = () => {
+//   aboutUs.style.display = "block";
+//   products.style.display = "block";
+//   login.style.display = "none";
+//   logout.style.display = "block";
+//   loginForm.style.display = "none";
+// };
 
 //  STICKY HEADER
 
